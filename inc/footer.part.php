5b686803ab184ceacec5b4bcfb3d9f96e1240078
Sun Junwen <Sun Junwen@71a53ac2-ec02-574d-9d89-1a8f02e6359e>
2011-01-18 10:44:10 +0000

    添加其他作品连接
    git-svn-id: file:///c/sunjw.doc/Programs/svn/JSMinNppSite_svn@8 71a53ac2-ec02-574d-9d89-1a8f02e6359e

--- inc/footer.part.php
+++ inc/footer.part.php
@@ -4,5 +4,6 @@
 <div>
 	<p>Except where otherwise noted, content on this site is licensed under the
 <a href="http://creativecommons.org/licenses/by-sa/3.0/" target="_blank">Creative Commons Attribution Share-Alike License v3.0</a>.</p>
+	<p><a href="https://addons.mozilla.org/en-us/firefox/addon/161901/" target="_blank">Encrypted Google Search for Firefox</a>&nbsp;|&nbsp;<a href="http://sourceforge.net/projects/rosefinch/" target="_blank">Rosefinch - Web file manager</a></p>
 	<p>2010-2011 <a href="http://sunblog.72pines.com" target="_blank">Sun Junwen</a>&nbsp;|&nbsp;sunjw8888 at gmail.com</p>
 </div>
