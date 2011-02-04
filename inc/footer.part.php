cba95c1d2d0919065a40a08433520b39e1201796
Sun Junwen <Sun Junwen@71a53ac2-ec02-574d-9d89-1a8f02e6359e>
2011-02-04 03:08:27 +0000

    添加 twitter fb 和人人连接
    git-svn-id: file:///c/sunjw.doc/Programs/svn/JSMinNppSite_svn@17 71a53ac2-ec02-574d-9d89-1a8f02e6359e

--- inc/footer.part.php
+++ inc/footer.part.php
@@ -5,5 +5,5 @@
 	<p>Except where otherwise noted, content on this site is licensed under the
 <a href="http://creativecommons.org/licenses/by-sa/3.0/" target="_blank">Creative Commons Attribution Share-Alike License v3.0</a>.</p>
 	<p><a href="https://addons.mozilla.org/en-us/firefox/addon/161901/" target="_blank">Encrypted Google Search for Firefox</a>&nbsp;|&nbsp;<a href="http://sourceforge.net/projects/rosefinch/" target="_blank">Rosefinch - Web file manager</a></p>
-	<p>2010-2011 <a href="http://sunblog.72pines.com" target="_blank">Sun Junwen</a>&nbsp;|&nbsp;sunjw8888 at gmail.com</p>
+	<p>2010-2011 <a href="http://sunblog.72pines.com" target="_blank">Sun Junwen</a>&nbsp;|&nbsp;sunjw8888 at gmail.com&nbsp;|&nbsp;<a href="http://www.twitter.com/sunjw" target="_blank" title="Follow me on Twitter"><img src="imgs/twitter-small.png" alt="Follow me on Twitter"/></a>&nbsp;<a href="http://www.facebook.com/profile.php?id=1444809914" target="_blank" title="Follow me on Facebook"><img src="imgs/fb-small.png" alt="Follow me on Facebook"/></a>&nbsp;<a href="http://www.renren.com/sunjwvista" target="_blank" title="Follow me on Renren"><img src="imgs/renren-small.png" alt="Follow me on Renren"/></a></p>
 </div>
