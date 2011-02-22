b47e04307627abf191430313ff77ca9fca04b822
Sun Junwen <Sun Junwen@71a53ac2-ec02-574d-9d89-1a8f02e6359e>
2011-02-22 12:56:28 +0000

    增加 xhtml validation
    git-svn-id: file:///c/sunjw.doc/Programs/svn/JSMinNppSite_svn@23 71a53ac2-ec02-574d-9d89-1a8f02e6359e

--- inc/footer.part.php
+++ inc/footer.part.php
@@ -4,5 +4,9 @@
 <div>
 	<p>Except where otherwise noted, content on this site is licensed under the
 <a href="http://creativecommons.org/licenses/by-sa/3.0/" target="_blank">Creative Commons Attribution Share-Alike License v3.0</a>.</p>
-	<p>2010-2011 <a href="http://sunblog.72pines.com" target="_blank">Sun Junwen</a>&nbsp;|&nbsp;sunjw8888 at gmail.com&nbsp;|&nbsp;<a href="http://www.twitter.com/sunjw" target="_blank" title="Follow me on Twitter"><img src="imgs/twitter-small.png" alt="Follow me on Twitter"/></a>&nbsp;<a href="http://www.facebook.com/profile.php?id=1444809914" target="_blank" title="Follow me on Facebook"><img src="imgs/fb-small.png" alt="Follow me on Facebook"/></a>&nbsp;<a href="http://www.renren.com/sunjwvista" target="_blank" title="Follow me on Renren"><img src="imgs/renren-small.png" alt="Follow me on Renren"/></a></p>
+	<p>2010-2011 <a href="http://sunblog.72pines.com" target="_blank">Sun Junwen</a>&nbsp;|&nbsp;sunjw8888 at gmail.com&nbsp;|&nbsp;<a href="http://www.twitter.com/sunjw" target="_blank" title="Follow me on Twitter"><img src="imgs/twitter-small.png" alt="Follow me on Twitter"/></a>&nbsp;<a href="http://www.facebook.com/profile.php?id=1444809914" target="_blank" title="Follow me on Facebook"><img src="imgs/fb-small.png" alt="Follow me on Facebook"/></a>&nbsp;<a href="http://www.renren.com/sunjwvista" target="_blank" title="Follow me on Renren"><img src="imgs/renren-small.png" alt="Follow me on Renren"/></a>&nbsp;|&nbsp;
+		<a href="http://validator.w3.org/check?uri=referer"><img
+        src="imgs/valid_xhtml10_blue.png"
+        alt="Valid XHTML 1.0 Transitional" height="15" width="80" /></a>
+	</p>
 </div>
