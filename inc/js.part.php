32a25f602034ae28cea1c0fa954173096bf49a06
Sun Junwen <Sun Junwen@71a53ac2-ec02-574d-9d89-1a8f02e6359e>
2011-11-18 01:55:54 +0000

    格式化 js
    git-svn-id: file:///c/sunjw.doc/Programs/svn/JSMinNppSite_svn@64 71a53ac2-ec02-574d-9d89-1a8f02e6359e

--- inc/js.part.php
+++ inc/js.part.php
@@ -4,37 +4,40 @@
 <script type="text/javascript" src="js/jquery.lightbox-0.5.plus.js"></script>
 <script type="text/javascript">
 //<![CDATA[
-	$(function() {
-			$('a.lightBox').lightBox({
-				autoAdapt: true
-			});
-			
-			jqFolding.setup({
-				listSelector : ".foldingList",
-				triggerSelector : ".listHeader",
-				containerSelector : ".foldingContainer"
-			});
-			jqFolding.init();
-
-			jqMenu.setup({
-				menuItemsSelector : ".menu",
-				menuButtonSelector : ".subToggle",
-				subMenuSelector : ".submenu",
-				inlineShadow : "transparent",
-				hoverOpen : true
-			});
-			$(jqMenu.init);
+$(function () {
+	$('a.lightBox').lightBox({
+		autoAdapt : true
 	});
 	
-	var _gaq = _gaq || [];
-	_gaq.push(['_setAccount', 'UA-5927560-3']);
-	_gaq.push(['_trackPageview']);
+	jqFolding.setup({
+		listSelector : ".foldingList",
+		triggerSelector : ".listHeader",
+		containerSelector : ".foldingContainer"
+	});
+	jqFolding.init();
+	
+	jqMenu.setup({
+		menuItemsSelector : ".menu",
+		menuButtonSelector : ".subToggle",
+		subMenuSelector : ".submenu",
+		inlineShadow : "transparent",
+		hoverOpen : true
+	});
+	$(jqMenu.init);
+});
 
-	(function() {
-			var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-			ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-			var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-	})();
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-5927560-3']);
+_gaq.push(['_trackPageview']);
+
+(function () {
+	var ga = document.createElement('script');
+	ga.type = 'text/javascript';
+	ga.async = true;
+	ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+	var s = document.getElementsByTagName('script')[0];
+	s.parentNode.insertBefore(ga, s);
+})();
 //]]>
 </script>
 	
