08cef6c58a489995284c9c6c7c8c85b904050650
Sun Junwen <Sun Junwen@71a53ac2-ec02-574d-9d89-1a8f02e6359e>
2013-02-03 12:24:48 +0000

    增加图标
    git-svn-id: file:///c/sunjw.doc/Programs/svn/JSMinNppSite_svn@88 71a53ac2-ec02-574d-9d89-1a8f02e6359e

--- inc/nav.part.php
+++ inc/nav.part.php
@@ -5,11 +5,14 @@
 			<ul class="submenuContainer">
 				<li><a href="download.php">Download</a></li>
 				<li><a href="screenshots.php">Screenshots</a></li>
-				<li class="split"><a href="help.php">Help</a></li>
+				<li><a href="help.php">Help</a></li>
 				<li><a href="http://www.sunjw.us" target="_blank">Blog</a></li>
 				<li><a href="http://tetris5.sf.net" target="_blank">Tetris5</a></li>
 			</ul>
 		</div>
 	</li>
 </ul>
+<div id="icon">
+	<img src="imgs/icon_048.png" alt="icon"/>
+</div>
 	
