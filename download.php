543c09058f497593187c8290a3e6d73107c763e7
Sun Junwen <Sun Junwen@71a53ac2-ec02-574d-9d89-1a8f02e6359e>
2015-11-10 11:47:54 +0000

    Change text in download page.
    git-svn-id: file:///c/sunjw.doc/Programs/svn/JSMinNppSite_svn@108 71a53ac2-ec02-574d-9d89-1a8f02e6359e

--- download.php
+++ download.php
@@ -84,7 +84,7 @@ if($checking) {
 							<p>
 							<?php 
 							if($is_lastest)
-								echo "You are using the latest version of JSToolNpp. May be you want to <a href='https://www.paypal.com/cgi-bin/webscr?cmd=_donations&amp;business=sunjw8888%40yahoo%2ecom%2ecn&amp;item_name=JSToolNpp%20Project&amp;no_shipping=1&amp;cn=Optional%20comments&amp;tax=0&amp;currency_code=USD&amp;lc=US&amp;amount=1.8&amp;bn=PP%2dDonationsBF&amp;charset=UTF%2d8' target='_blank'>Donate</a>.";
+								echo "Congratulations! You are using the latest version of JSToolNpp.";
 							else
 								echo "You are <strong>NOT</strong> using the latest version of JSToolNpp. Download new version below.";
 							?>
