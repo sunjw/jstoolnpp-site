afba78297def740d6c5a76c49e7f1233e53bd20e
Sun Junwen <sunjw8888@gmail.com>
2018-11-05 16:23:53 +0800

    Comment out GA code for dev.

--- inc/js.part.php
+++ inc/js.part.php
@@ -56,30 +56,20 @@ $(function () {
     onPageScroll();
 });
 
+/*
 var _gaq = _gaq || [];
 _gaq.push(['_setAccount', 'UA-5927560-3']);
 _gaq.push(['_trackPageview']);
 
 (function () {
-    var ga = document.createElement('script');
-    ga.type = 'text/javascript';
-    ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0];
-    s.parentNode.insertBefore(ga, s);
+var ga = document.createElement('script');
+ga.type = 'text/javascript';
+ga.async = true;
+ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+var s = document.getElementsByTagName('script')[0];
+s.parentNode.insertBefore(ga, s);
 })();
+ */
 
-window.___gcfg = {
-    lang: 'zh-CN'
-};
-
-(function () {
-    var po = document.createElement('script');
-    po.type = 'text/javascript';
-    po.async = true;
-    po.src = 'https://apis.google.com/js/plusone.js';
-    var s = document.getElementsByTagName('script')[0];
-    s.parentNode.insertBefore(po, s);
-})();
 //]]>
 </script>
