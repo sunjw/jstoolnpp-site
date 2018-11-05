c9b8acbd0b950a793b7a5bf732fd8bec4b2a7181
Sun Junwen <sunjw8888@gmail.com>
2018-11-05 17:04:59 +0800

    Remove menu.css.

--- inc/head.part.php
+++ inc/head.part.php
@@ -3,5 +3,4 @@
 <link rel="shortcut icon" href="http://www.sunjw.us/jstoolnpp/favicon.ico" type="image/vnd.microsoft.icon">
 <link rel="icon" href="http://www.sunjw.us/jstoolnpp/favicon.ico" type="image/vnd.microsoft.icon">
 <link href="../css/style.css" rel="stylesheet" type="text/css" />
-<link href="../css/menu.css" rel="stylesheet" type="text/css" />
 	
