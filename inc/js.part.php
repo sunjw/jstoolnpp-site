9337b1e4b092bd1392ea090655e1c11400e26ff5
Sun Junwen <sunjw8888@gmail.com>
2018-11-05 16:41:44 +0800

    Upgrade jquery to 3.3.1.

--- inc/js.part.php
+++ inc/js.part.php
@@ -1,4 +1,4 @@
-<script type="text/javascript" src="../js/jquery-1.4.4.min.js"></script>
+<script type="text/javascript" src="../js/jquery-3.3.1.min.js"></script>
 <script type="text/javascript">
 //<![CDATA[
 function getScrollTop() {
