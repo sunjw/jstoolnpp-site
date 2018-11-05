1fb8a49fa2a31f533eb02788b3542ad1c2dc70c0
Sun Junwen <sunjw8888@gmail.com>
2018-11-05 16:35:29 +0800

    Init npp page.

--- inc/head.part.php
+++ inc/head.part.php
@@ -2,7 +2,7 @@
 <meta name="keywords" content="JSToolNpp, JSMinNpp, Notepad++, npp, JavaScript, js, jsmin, js minimize, js format, json, json viewer, sourceforge, sf.net, google code, open source, Sun Junwen, sunjw8888" />
 <link rel="shortcut icon" href="http://www.sunjw.us/jstoolnpp/favicon.ico" type="image/vnd.microsoft.icon">
 <link rel="icon" href="http://www.sunjw.us/jstoolnpp/favicon.ico" type="image/vnd.microsoft.icon">
-<link href="css/style.css" rel="stylesheet" type="text/css" />
-<link href="css/jquery.lightbox-0.5.css" rel="stylesheet" type="text/css" />
-<link href="css/menu.css" rel="stylesheet" type="text/css" />
+<link href="../css/style.css" rel="stylesheet" type="text/css" />
+<link href="../css/jquery.lightbox-0.5.css" rel="stylesheet" type="text/css" />
+<link href="../css/menu.css" rel="stylesheet" type="text/css" />
 	
--- inc/js.part.php
+++ inc/js.part.php
@@ -1,6 +1,6 @@
-<script type="text/javascript" src="js/jquery-1.4.4.min.js"></script>
-<script type="text/javascript" src="js/jquery.folding.min.js"></script>
-<script type="text/javascript" src="js/jquery.lightbox-0.5.plus.js"></script>
+<script type="text/javascript" src="../js/jquery-1.4.4.min.js"></script>
+<script type="text/javascript" src="../js/jquery.folding.min.js"></script>
+<script type="text/javascript" src="../js/jquery.lightbox-0.5.plus.js"></script>
 <script type="text/javascript">
 //<![CDATA[
 function getScrollTop() {
