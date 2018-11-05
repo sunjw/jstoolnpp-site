e77221efc3d37bce721a14f747b693c77ec459ba
Sun Junwen <sunjw8888@gmail.com>
2018-11-05 16:39:01 +0800

    Remove useless js.

--- inc/js.part.php
+++ inc/js.part.php
@@ -1,5 +1,4 @@
 <script type="text/javascript" src="../js/jquery-1.4.4.min.js"></script>
-<script type="text/javascript" src="../js/jquery.folding.min.js"></script>
 <script type="text/javascript">
 //<![CDATA[
 function getScrollTop() {
@@ -31,13 +30,6 @@ function onPageScroll() {
 }
 
 $(function () {
-    jqFolding.setup({
-        listSelector: ".foldingList",
-        triggerSelector: ".listHeader",
-        containerSelector: ".foldingContainer"
-    });
-    jqFolding.init();
-
     pageWrapper = $(".pageWrapper");
     navWrapper = $("#divHeader #navWrapper");
 
