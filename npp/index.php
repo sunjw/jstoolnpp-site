c7c0c19b82d34f9044beeb007e8b54672e2bf270
Sun Junwen <sunjw8888@gmail.com>
2018-11-07 14:14:50 +0800

    Help content.

--- npp/index.php
+++ npp/index.php
@@ -76,7 +76,7 @@ if ($checking) {
                         <li>A JSON data viewer. This JSON data viewer can handle >10MB JSON file easily.</li>
                         <li>Support 64bit Notepad++ (from version 1.20.0, ".64.zip" package).</li>
                     </ul>
-                    <p>Really helpful to JavaScript coder on Notepad++ and really easy to use it.</p>
+                    <p>Really helpful to JavaScript coder on Notepad++ and really easy to <a class="smoothAnchor" href="#help">use</a> it.</p>
                     <p>&nbsp;</p>
                     <p>Made in China.</p>
                     <p>License: <a href="http://www.gnu.org/licenses/gpl-2.0.html" target="_blank">GPL 2.0</a></p>
@@ -124,16 +124,16 @@ if ($checking) {
                     <ul class="indentList discList">
                         <li>
                             <div>
-                            <p>Unicode build:</p>
-                            <p>32bit - JSToolNPP.<?php echo $cur_version; ?>.uni.32.zip:&nbsp;<a href="https://sourceforge.net/projects/jsminnpp/files/Uni/JSToolNPP.<?php echo $cur_version; ?>.uni.32.zip/download" target="_blank">Download from SourceForget.net</a></p>
-                            <p>64bit - JSToolNPP.<?php echo $cur_version; ?>.uni.64.zip:&nbsp;<a href="https://sourceforge.net/projects/jsminnpp/files/Uni/JSToolNPP.<?php echo $cur_version; ?>.uni.64.zip/download" target="_blank">Download from SourceForget.net</a></p>
+                                <p>Unicode build:</p>
+                                <p>32bit - JSToolNPP.<?php echo $cur_version; ?>.uni.32.zip:&nbsp;<a href="https://sourceforge.net/projects/jsminnpp/files/Uni/JSToolNPP.<?php echo $cur_version; ?>.uni.32.zip/download" target="_blank">Download from SourceForget.net</a></p>
+                                <p>64bit - JSToolNPP.<?php echo $cur_version; ?>.uni.64.zip:&nbsp;<a href="https://sourceforge.net/projects/jsminnpp/files/Uni/JSToolNPP.<?php echo $cur_version; ?>.uni.64.zip/download" target="_blank">Download from SourceForget.net</a></p>
                             </div>
                         </li>
                         <li>
                             <div>
-                            <p>ASCII build (<em>no more updates after 1.20.2</em>):</p>
-                            <p>32bit - JSToolNPP.1.20.2.asc.32.zip:&nbsp;<a href="https://sourceforge.net/projects/jsminnpp/files/Asc/JSToolNPP.1.20.2.asc.32.zip/download" target="_blank">Download from SourceForget.net</a></p>
-                            <p>64bit - JSToolNPP.1.20.2.asc.64.zip:&nbsp;<a href="https://sourceforge.net/projects/jsminnpp/files/Asc/JSToolNPP.1.20.2.asc.64.zip/download" target="_blank">Download from SourceForget.net</a></p>
+                                <p>ASCII build (<em>no more updates after 1.20.2</em>):</p>
+                                <p>32bit - JSToolNPP.1.20.2.asc.32.zip:&nbsp;<a href="https://sourceforge.net/projects/jsminnpp/files/Asc/JSToolNPP.1.20.2.asc.32.zip/download" target="_blank">Download from SourceForget.net</a></p>
+                                <p>64bit - JSToolNPP.1.20.2.asc.64.zip:&nbsp;<a href="https://sourceforge.net/projects/jsminnpp/files/Asc/JSToolNPP.1.20.2.asc.64.zip/download" target="_blank">Download from SourceForget.net</a></p>
                             </div>
                         </li>
                     </ul>
@@ -160,14 +160,92 @@ if ($checking) {
                     <div class="listHeaderTitle"><a name="help" title="Help"></a>Help</div>
                 </div>
                 <div class="listContainer">
-                    <p>You can install JSToolNpp through Plugin Manager in Notepad++:</p>
-                    <ul class="indentList">
-                        <li>1. Run Notepad++.</li>
-                        <li>2. Open menu: "Plugins" - "Plugin Manager" - "Show Plugin Manager".</li>
-                        <li>3. Under "Available" tab, select "JSTool".</li>
-                        <li>4. Click "Install" button. Then Plugin Manager will do downloading and installing automatically.</li>
+                    <ul class="indentList discList">
+                        <li>
+                            <div>
+                                <p>Install through Plugin Manager</p>
+                                <ol class="indentList">
+                                    <li>Run Notepad++.</li>
+                                    <li>Open menu: "Plugins" - "Plugin Manager" - "Show Plugin Manager". If you can't find "Plugin Manager" in your Notepad++ menu, please go to <a href="https://github.com/bruderstein/nppPluginManager" target="_blank">Notepad++ Plugin Manager</a>, download and install "Plugin Manager" manually.</li>
+                                    <li>Under "Available" tab, select "JSTool".</li>
+                                    <li>Click "Install" button. Then Plugin Manager will do downloading and installing automatically.</li>
+                                </ol>
+                            </div>
+                        </li>
                     </ul>
-                    <p>Or you can <a href="download.php">download JSToolNpp</a> and install it manually. <a href="help.php#Install_manually">See Help...</a></p>
+                    <p>&nbsp;</p>
+                    <ul class="indentList discList">
+                        <li>
+                            <div>
+                                <p>Install manually</p>
+                                <ol class="indentList">
+                                    <li><a class="smoothAnchor" href="#download">Download JSToolNpp</a>.</li>
+                                    <li>Extract JSMinNpp.dll from JSToolNpp.x.y.z.uni.zip (or JSToolNpp.x.y.z.asc.zip) you just downloaded.</li>
+                                    <li>Copy JSMinNpp.dll to plugins directory under your Notepad++ installation path, like "C:\Program Files (x86)\Notepad++\plugins\".</li>
+                                    <li>Run Notepad++, you will see JSTool in "Plugins" menu.</li>
+                                </ol>
+                            </div>
+                        </li>
+                    </ul>
+                    <p>&nbsp;</p>
+                    <ul class="indentList discList">
+                        <li>
+                            <div>
+                                <p>Uninstall JSToolNpp</p>
+                                <p>Uninstall through Plugin Manager:</p>
+                                <ol class="indentList">
+                                    <li>Run Notepad++.</li>
+                                    <li>Open menu: "Plugins" - "Plugin Manager" - "Show Plugin Manager".</li>
+                                    <li>Under "Installed" tab, select "JSTool".</li>
+                                    <li>Click "Remove" button. Then Plugin Manager will do uninstalling automatically.</li>
+                                </ol>
+                                <p>&nbsp;</p>
+                                <p>Or you can uninstall manually:</p>
+                                <ol class="indentList">
+                                    <li>Exit any running Notepad++.</li>
+                                    <li>Go to plugins directory under your Notepad++ installation path, like "C:\Program Files (x86)\Notepad++\plugins\".</li>
+                                    <li>Delete JSMinNpp.dll.</li>
+                                </ol>
+                            </div>
+                        </li>
+                    </ul>
+                    <p>&nbsp;</p>
+                    <ul class="indentList discList">
+                        <li>
+                            <div>
+                                <p>How to use JSToolNpp</p>
+                                <p>Minimize source code:</p>
+                                <ol class="indentList">
+                                    <li>Open you JavaScript source code file in Notepad++.</li>
+                                    <li>Open menu: "Plugins" - "JSTool".</li>
+                                    <li>If you want to put minimized source code in current file, click "JSMin". Or if you want to put minimized source code in a new file, click "JSMin (New file)".</li>
+                                </ol>
+                                <p>&nbsp;</p>
+                                <p>Format all code in a source code file:</p>
+                                <ol class="indentList">
+                                    <li>Open you JavaScript source code file in Notepad++.</li>
+                                    <li>Press "Ctrl"+"Alt"+"M".</li>
+                                    <li>Or use menu, open menu: "Plugins" - "JSTool" then click "JSFormat".</li>
+                                </ol>
+                                <p>&nbsp;</p>
+                                <p>Format selected code in a source code file:</p>
+                                <ol class="indentList">
+                                    <li>Open you JavaScript source code file in Notepad++.</li>
+                                    <li>Select source code lines which you want to format.</li>
+                                    <li>Press "Ctrl"+"Alt"+"M".</li>
+                                    <li>Or use menu, open menu: "Plugins" - "JSTool" then click "JSFormat".</li>
+                                </ol>
+                                <p>&nbsp;</p>
+                                <p>Json Viewer:</p>
+                                <ol class="indentList">
+                                    <li>Open you JavaScript or json source code file in Notepad++.</li>
+                                    <li>Press "Ctrl"+"Alt"+"J".</li>
+                                    <li>Or use menu, open menu: "Plugins" - "JSTool" then click "Json Viewer".</li>
+                                    <li>Or click <img src="../imgs/icon_toolbar.png"/> on toolbar.</li>
+                                </ol>
+                            </div>
+                        </li>
+                    </ul>
                 </div>
             </div>
             <div id="divFooter" class="center">
