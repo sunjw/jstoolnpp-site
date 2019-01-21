c50ac76ece278caf04a01205eed4c35f375b34f1
Sun Junwen <sunjw8888@gmail.com>
2019-01-21 12:56:08 +0800

    Add redirect source to url parameter.

--- inc/navtonewsite.php
+++ inc/navtonewsite.php
@@ -2,14 +2,19 @@
 include_once "common.inc.php";
 
 $new_site_location = "Location: ../jstool/";
+$redirect_src_here = "jstoolnpp";
 
 $request_path = $_SERVER["PHP_SELF"];
 $request_file = basename($request_path);
-if ($request_file == "download.php") {
-    $ver = get_query("ver");
-    if ($ver != "") {
-        $new_site_location = $new_site_location."npp/?ver=".$ver;
+$ver = get_query("ver");
+if ($request_file == "download.php" && $ver != "") {
+    $new_site_location = $new_site_location."npp/?redirect_src=".$redirect_src_here."&ver=".$ver;
+} else {
+    $redirect_src = get_query("redirect_src");
+    if ($redirect_src == "") {
+        $redirect_src = $redirect_src_here;
     }
+    $new_site_location = $new_site_location."?redirect_src=".$redirect_src;
 }
 
 header($new_site_location);
